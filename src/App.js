fb30ead57be064f4a6d5e7d36dc72e8d1c353eac
unknown <blesseddominic98@gmail.com>
2020-07-28 07:21:54 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './homepage/homepage.component';
 
+const Hatspage = (props) => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
+// const TopicDetail = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <button onClick={() => props.history.push('/topics')}>Topics</button>
+//       <h1>TopicDetail PAGE</h1>
+//     </div>
+//   );
+// };
+
+// const TopicsList = (props) => (
+//   <div>
+//     <h1>Topic List PAGE</h1>
+//     {/* <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
+//     <Link to={`${props.match.url}/13`}>TO TOPIC 17</Link>
+//     <Link to={`${props.match.url}/13`}>TO TOPIC 21</Link> */}
+//   </div>
+// );
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={Hatspage} />
+        {/* <Route exact path="/topics" component={TopicsList} />
+        <Route exact path="/Topics/:topicId" component={TopicDetail} /> */}
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div
+const MenuItem = ({ title, imageUrl, size, history, linkUrl,match }) => (
+
+    <div
     style={{ backgroundImage: `url(${imageUrl})` }}
-    className={`${size} menu-item`}
-  >
-    <div className="content">
+    className={`${size} menu-item background-image`} onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
-  </div>
+    </div>
+    // <div className="content">
+    //   <h1 className="title">{title.toUpperCase()}</h1>
+    //   <span className="subtitle">SHOP NOW</span>
+    // </div>
+
+
 );
 
-export default MenuItem;
+export default  withRouter(MenuItem);
